a29a456fd4e003e901f7fa5f54391ae6f39be2b0
kibeomkim <gbkim@gsinfo.kr>
2021-04-05 18:04:44 +0900

    second commit

--- src/main/java/com/kibeom/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/kibeom/springboot/web/dto/HelloResponseDto.java
@@ -3,7 +3,10 @@ package com.kibeom.springboot.web.dto;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDto {
 
-
+    private final String name;
+    private final int amount;
 }
