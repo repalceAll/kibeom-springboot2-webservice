3abbc2cecb8eb091e94aae9b7708b62d53223f2e
kibeomkim <gbkim@gsinfo.kr>
2022-11-15 13:54:47 +0900

    second commit

--- src/main/java/com/kibeom/springboot/Application.java
+++ src/main/java/com/kibeom/springboot/Application.java
@@ -2,9 +2,10 @@ package com.kibeom.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import static org.springframework.boot.autoconfigure.SpringBootApplication.*;
 
+@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
